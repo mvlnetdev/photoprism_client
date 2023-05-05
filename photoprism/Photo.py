144b2577c279e9dac936f696eaae2ccdf189267f
Francesco <francescoscuccimarri@gmail.com>
2023-05-05 09:00:26 +0200

    minor fix

--- photoprism/Photo.py
+++ photoprism/Photo.py
@@ -14,7 +14,7 @@ class Photo():
             "X-Session-ID": self.session.session_id}
 
 
-    def search(self, query, count=100, offset=0, order='newest'):
+    def search(self, query, count=100, offset=0, order="newest"):
         """Basic search function. Returns a Dict object based upon the returned JSON"""
 
         url = f"{self.session.url}/photos?count={count}&q={query}&offset={offset}&order={order}"
