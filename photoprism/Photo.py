8b5bbd2870aa894f3cfb33cbb12c82bc3c94c0a4
Maarten van Leeuwen <maartenvanleeuwen1996@gmail.com>
2023-05-05 18:03:12 +0200

    Changed all functions so it uses Session.req.

--- photoprism/Photo.py
+++ photoprism/Photo.py
@@ -10,16 +10,11 @@ class Photo():
             raise TypeError(f"session variable is not of type photoprism.Session.Session, but {type(session)}")
 
         self.session = session
-        self.header = {
-            "X-Session-ID": self.session.session_id}
-
 
     def search(self, query, count=100, offset=0, order="newest"):
         """Basic search function. Returns a Dict object based upon the returned JSON"""
-
-        url = f"{self.session.url}/photos?count={count}&q={query}&offset={offset}&order={order}"
-        r = requests.get(url, headers=self.header)
-        return json.loads(r.text)
+        _, data = self.session.req(f"/photos?count={count}&q={query}&offset={offset}&order={order}", "GET")
+        return data
 
     def get_uid_list_of_search(self, query, count=100):
         """Return a list of UIDs based upon the search"""
@@ -31,12 +26,10 @@ class Photo():
 
         return photolist
 
-    def list_albums(self):
+    def list_albums(self, count=100000):
         """Provide a list of all albums within the photoprism instance, with a max of 100000"""
-
-        url = f"{self.session.url}/albums?count=100000"
-        r = requests.get(url, headers=self.header)
-        return json.loads(r.text)
+        _, data = self.session.req(f"/albums?count={count}", "GET")
+        return data
 
     def check_if_album_exists(self, name, create_if_not=False):
         """Small function to check if an album exists"""
@@ -64,111 +57,83 @@ class Photo():
 
     def create_album(self, title):
         """Create an album, returns a boolean if it worked"""
-
-        url = f"{self.session.url}/albums"
         data = {"Title":title,"Favorite":False}
-        r = requests.post(url=url, data=json.dumps(data), headers=self.header)
-        
-        if r.status_code == 200:
-            return True
+        status_code, output = self.session.req("/albums", "POST", data=data)
+
+        if status_code == 200:
+            return output
         
         return False
 
     def add_photos_to_album(self, photos, album_uid):
         """Add photos to an album, you will need to provide a list of UIDs of the photos you want to add. Returns True if successfull"""
-
-        url = f"{self.session.url}/albums/{album_uid}/photos"
         data = {
             "photos":photos
         }
-        r = requests.post(url, data=json.dumps(data), headers=self.header)
-        
-        if r.status_code == 200:
+        status_code, _ = self.session.req(f"/albums/{album_uid}/photos", "POST", data=data)
+        if status_code == 200:
             return True
 
         return False
 
-    def add_to_album_from_query(self, query, albumname):
+    def add_to_album_from_query(self, query, albumname, count=1000000):
         """Provide a search query and add all photos that are returned into an album. Provide the albumname, not the UID of the album."""
 
         self.check_if_album_exists(albumname, create_if_not=True)
         album_uid = self.get_album_uid_by_name(albumname)
-        photolist = self.get_uid_list_of_search(query, count=1000000)
+        photolist = self.get_uid_list_of_search(query, count=count)
         result = self.add_photos_to_album(photolist, album_uid)
         return result
 
     def get_album(self, uid):
         """Get all information of an album based upon the UID of the album"""
+        status_code, data = self.session.req(f"/albums/{uid}", "GET")
+        if status_code == 200:
+            return data
 
-        url = f"{self.session.url}/albums/{uid}"
-        r = requests.get(url, headers=self.header)
-        
-        if r.status_code == 200:
-            return json.loads(r.text)
-        
         return False
 
-    def remove_photos_from_album(self,albumname, photos=False, count=1000000):
+    def remove_photos_from_album(self, albumname, photos=False, count=1000000):
         """Remove photos from an album, Returns True if successfull"""
         album_uid = self.get_album_uid_by_name(albumname)
         if photos == False:
             query = f"album:\"{albumname}\""
             photos = self.get_uid_list_of_search(query,count=count)
 
-        url = f"{self.session.url}/albums/{album_uid}/photos"
         data = {
             "photos":photos
         }
-        r = requests.delete(url, data=json.dumps(data), headers=self.header)
-        result = False
-        if r.status_code == 200:
-            result = True
-        return result
+        status_code, _ = self.session.req(f"/albums/{album_uid}/photos", "DELETE", data=data)
+        if status_code == 200:
+            return True
+
+        return False
 
     def remove_album(self, albumname):
         """Remove album based on album name"""
         album_uid = self.get_album_uid_by_name(albumname)
-        url = f"{self.session.url}/albums/{album_uid}"
-        r = requests.delete(url, headers=self.header)
-        result = False
-        if r.status_code == 200:
-            result = json.loads(r.text)
-        return result
+        status_code, data = self.session.req(f"/albums/{album_uid}", "DELETE")
+        if status_code == 200:
+            return data
+
+        return False
 
     def start_import(self, path="upload", move=False):
         """Start an import job, default path is upload. It returns True when the import started, not when finished"""
-
-        url = f"{self.session.url}/import"
         data = {
             "path": path,
             "move": move
         }
-        r = requests.post(url, data=json.dumps(data), headers=self.header)
-        
-        if r.status_code == 200:
+        status_code, _ = self.session.req(f"/import", "POST", data=data)
+        if status_code == 200:
             return True
         
         return False
 
     def stop_import(self):
         """Stop an import job"""
-        url = f"{self.session.url}/import"
-        r = requests.delete(url, headers=self.header)
-
-        if r.status_code == 200:
+        status_code, _ = self.session.req("/import", "DELETE")
+        if status_code == 200:
             return True
-        
-        return False
-
-    def raw_call(self, endpoint, type="GET", data=None):
-        """Function to perform a request to the photoprism server"""
-
-        url = f"{self.session.url}/{endpoint}"
-        if type == "GET":
-            return requests.get(url, headers=self.header)
-        elif type == "POST":
-            return requests.post(url, data=json.dumps(data) if data else None, headers=self.header)
-        elif type == "DELETE":
-            return requests.delete(url, headers=self.header)
         
         return False
