5f0727f4fdf9c396e49db708d401d14cac35473d
Maarten van Leeuwen <maartenvanleeuwen1996@gmail.com>
2023-05-06 09:49:26 +0200

    Added additional filetypes

--- photoprism/mimetypes.py
+++ photoprism/mimetypes.py
@@ -1,4 +1,11 @@
+# Containing common image formats.
+# Source: https://developer.mozilla.org/en-US/docs/Web/HTTP/Basics_of_HTTP/MIME_types/Common_types
 image = {
     "jpeg": ".jpg",
-    "png": ".png"
+    "png": ".png",
+    "avif": ".avif",
+    "bmp": ".bmp",
+    "gif": ".gif",
+    "svg+xml": ".svg",
+    "tiff": ".tff"
 }
