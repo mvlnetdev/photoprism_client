f6ef7dccb640e0a20f120c497ac9d5c68e649fe4
Maarten van Leeuwen <maartenvanleeuwen1996@gmail.com>
2023-05-06 13:49:27 +0200

    Added an example of two new functions

--- examples/download_files.py
+++ examples/download_files.py
@@ -8,3 +8,8 @@ p = Photo(pp_session)
 data = p.search(query="original:*", count=1)
 
 p.download_file(data[0]["Hash"])
+
+data = p.list_albums(count=1)
+p.download_album(uid=data[0]["UID"])
+
+p.download_files_from_query(query="original:*", count=1)
